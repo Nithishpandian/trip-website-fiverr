6bceaa0abd922137d4aeb080ab1e5e7540a1f3e8
Nithishpandian <nithishpandiyan75@gmail.com>
2023-10-30 01:22:32 +0530

    verified otp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
-import { React } from "react";
+import { React, useState } from "react";
 import "./../assets/styles/hero.css";
 import CountUp from "react-countup";
 import { motion } from "framer-motion";
+import Popup from "../components/Popup";
 
 const Home = () => {
+  const [open, setOpen] = useState(false);
+  const [step, setStep] = useState(0);
+
   return (
     <section className="hero-wrapper">
       <div className=" hero-container">
@@ -31,7 +35,21 @@ const Home = () => {
           {/* Connect Button */}
 
           <div className="contactus">
-            <button className="button">Customize Now</button>
+            <button
+              className="button"
+              onClick={() => {
+                setStep(1);
+                setOpen(true);
+              }}
+            >
+              Customize Now
+            </button>
+            <Popup
+              open={open}
+              setOpen={setOpen}
+              step={step}
+              setStep={setStep}
+            />
           </div>
 
           <div className=" stats">
